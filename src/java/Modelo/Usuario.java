d09e925373bf5bb919772aec57da7f2e44a3f82f
luisspider <luishumbertor155@gmail.com>
2021-05-27 19:00:55 -0500

    1

--- src/java/Modelo/Usuario.java
+++ src/java/Modelo/Usuario.java
@@ -1,14 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Modelo;
 
-/**
- *
- * @author JJ
- */
 public class Usuario {
-    
+    int idUsu;
+    String nombre;
+    String Apellido;
+    String edad;
+
+    public int getIdUsu() {
+        return idUsu;
+    }
+
+    public void setIdUsu(int idUsu) {
+        this.idUsu = idUsu;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String Apellido) {
+        this.Apellido = Apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
 }
