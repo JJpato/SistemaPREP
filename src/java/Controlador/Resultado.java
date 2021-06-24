9e408f1a1786ef13e92e35a75732e61075724a84
Raul <Raul@Raul>
2021-06-24 10:00:59 -0500

    menu

--- src/java/Controlador/Resultado.java
+++ src/java/Controlador/Resultado.java
@@ -118,6 +118,10 @@ public class Resultado extends HttpServlet {
         } catch (SQLException ex) {
             Logger.getLogger(Resultado.class.getName()).log(Level.SEVERE, null, ex);
         }
+        out.print("    <head>\n" +
+"        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0\" crossorigin=\"anonymous\">\n" +
+"    </head>");
+        out.print("<!--Cabecero --> <header id=\"main-header\" class=\"py-2 bg-secondary text-white\"> <div class=\"container\"> <div class=\"row\"> <div class=\"col-md-6\"> <a href=\"Inicio.jsp\" class=\"nav-item nav-link text-white\"> <h1> <i class=\"fas fa-balance-scale\"></i></i> Sistema PREP </h1> </a> </div> </div> </div> </header>");
         out.print("<h1>"+"VOTOS"+"</h1>");
             
             out.print("<table border='1'>");
